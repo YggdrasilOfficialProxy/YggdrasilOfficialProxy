3b8aae657e2b1650fad2e54bab027a01fa158fcf
Karlatemp <karlatemp@vip.qq.com>
2021-09-07 23:56:25 +0800

    Fix comments not rendering

--- src/main/kotlin/io/github/yggdrasilofficialproxy/YopConfiguration.kt
+++ src/main/kotlin/io/github/yggdrasilofficialproxy/YopConfiguration.kt
@@ -12,11 +12,12 @@ import org.spongepowered.configurate.objectmapping.ConfigSerializable
 import org.spongepowered.configurate.objectmapping.meta.Comment
 
 @ConfigSerializable
-data class YopConfiguration @JvmOverloads constructor(
-    @Comment("Proxy server host")
-    val host : ServerHost = ServerHost(),
-    @Comment("Yggdrasil servers using")
-    val yggdrasilServers : List<YggdrasilServerInfo> = listOf(
+data class YopConfiguration(
+    @field:Comment("Proxy server host")
+    @JvmField val host : ServerHost = ServerHost(),
+
+    @field:Comment("Yggdrasil servers using")
+    @JvmField val yggdrasilServers : List<YggdrasilServerInfo> = listOf(
         YggdrasilServerInfo(
             name = "mojang",
             proxy = YggdrasilServerInfo.ProxyInfo(
@@ -36,40 +37,48 @@ data class YopConfiguration @JvmOverloads constructor(
             )
         )
     ),
-    @Comment("Logger level, options: NONE, ERROR, WARN, INFO, DEBUG, TRACE, ALL")
-    val loggerLevel : LoggerLevel = LoggerLevel.INFO,
+
+    @field:Comment("Logger level, options: NONE, ERROR, WARN, INFO, DEBUG, TRACE, ALL")
+    @JvmField val loggerLevel : LoggerLevel = LoggerLevel.INFO,
 ) {
 
     @ConfigSerializable
-    data class YggdrasilServerInfo @JvmOverloads constructor(
-        @Comment("<Optional> When you use @ selector please fill it")
-        val name : String = "",
-        @Comment("<Required>")
-        val api : String = "",
-        @Comment("Proxy info")
-        val proxy : ProxyInfo = ProxyInfo()
+    data class YggdrasilServerInfo(
+        @field:Comment("<Optional> When you use @ selector please fill it")
+        @JvmField val name : String = "",
+
+        @field:Comment("<Required>")
+        @JvmField val api : String = "",
+
+        @field:Comment("Proxy info")
+        @JvmField val proxy : ProxyInfo = ProxyInfo()
     ) {
         @ConfigSerializable
-        data class ProxyInfo @JvmOverloads constructor(
-            @Comment("<Required> (direct, http, socks)")
-            val type : String = "direct",
-            @Comment("<Optional> Please fill in the [url] when you use http proxy")
-            val url : String? = null,
-            @Comment("<Optional> Please fill in the [host] when you use socks proxy")
-            val host : String? = null,
-            @Comment("<Optional> Please fill in the [port] when you use socks proxy")
-            val port : Int? = null,
-            @Comment("<Optional> Please fill in the [username] when this proxy needs to be authenticated")
-            val username : String? = null,
-            @Comment("<Optional> Please fill in the [password] when this proxy needs to be authenticated")
-            val password : String? = null
+        data class ProxyInfo(
+            @field:Comment("<Required> (direct, http, socks)")
+            @JvmField val type : String = "direct",
+
+            @field:Comment("<Optional> Please fill in the [url] when you use http proxy")
+            @JvmField val url : String? = null,
+
+            @field:Comment("<Optional> Please fill in the [host] when you use socks proxy")
+            @JvmField val host : String? = null,
+
+            @field:Comment("<Optional> Please fill in the [port] when you use socks proxy")
+            @JvmField val port : Int? = null,
+
+            @field:Comment("<Optional> Please fill in the [username] when this proxy needs to be authenticated")
+            @JvmField val username : String? = null,
+
+            @field:Comment("<Optional> Please fill in the [password] when this proxy needs to be authenticated")
+            @JvmField val password : String? = null,
         )
     }
 
     @ConfigSerializable
-    data class ServerHost @JvmOverloads constructor(
-        val host : String = "0.0.0.0",
-        val port : Int = 32217,
+    data class ServerHost(
+        @JvmField val host : String = "0.0.0.0",
+        @JvmField val port : Int = 32217,
     )
 
     enum class LoggerLevel {
